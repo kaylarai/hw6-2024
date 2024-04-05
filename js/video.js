26427e6dc31957e4e47402a79921f383a276846c
kaylarai <143545319+kaylarai@users.noreply.github.com>
2024-04-05 10:13:34 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,18 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
+	console.log("auto play is set to" + video.autoplay)
+	console.log("loop is set to" + video.loop)
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
